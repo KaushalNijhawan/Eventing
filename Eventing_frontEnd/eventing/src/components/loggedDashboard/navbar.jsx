c07d1ae1903204486674ed66186ff5cf9ad9dbbc
Kaushal Nijhawan <kaushalnijhawan3641@gmaial.com>
2022-10-31 08:57:19 +0000

    commit logout changes

--- Eventing_frontEnd/eventing/src/components/loggedDashboard/navbar.jsx
+++ Eventing_frontEnd/eventing/src/components/loggedDashboard/navbar.jsx
@@ -1,10 +1,25 @@
 import "./dasboard.css";
 import { Person } from '@mui/icons-material';
 import EventIcon from '@mui/icons-material/Event';
-import { Link } from "react-router-dom";
-import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { loggUser } from "../../Redux/resolvers/userResolver";
 const Navbar = (props) => {
     const {currentUser} = props;
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const logOutSession=()=>{
+        dispatch(loggUser({
+            ...currentUser,
+            username :"",
+            loggedIn:false,
+            token:'',
+            bookingids:[]
+        }));
+
+        navigate("/");
+    }
+
     return (
         <nav className="navbar fixed-top navbar-expand-lg navbar-light" style={{backgroundColor: 'rgb(15, 82, 186)'}}>
             <EventIcon className="navbar-brand" style={{ fontSize: "45px", color: 'white' }} />
@@ -27,7 +42,7 @@ const Navbar = (props) => {
             </div>
             <div className="navEnd-text">
                 <span>{props.currentUser && props.currentUser.username ? props.currentUser.username : ''}<Person /></span>
-                <a className="navbar-brand" href="#" style={{ color: 'white' }}>Logout</a>
+                <a className="navbar-brand" href="#" style={{ color: 'white' }} onClick = {()=> logOutSession()}>Logout</a>
             </div>
         </nav>
     );
